2a752f0ba06ef20063a537c5072c12ba14fd7496
Girka-Kirill <90462891+Girka-Kirill@users.noreply.github.com>
2021-09-17 21:01:59 +0300

    Add files via upload

---
+++ GirkaHW.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+void Split(char* string, char* delimiters, char*** tokens, int* tokensCount) {
+	char* tmp = string;
+	char* tmp2;
+	int len = strlen(string), i = 0, lenToken;
+	while (tmp != NULL) {
+		tmp = strpbrk(tmp, delimiters);
+		i++;
+		if (tmp == NULL) {
+			break;
+		}
+		tmp = tmp + 1;
+	}
+	*tokensCount = i;
+	i = 0;
+	*tokens = (char**) realloc(*tokens, *tokensCount * sizeof(char*));
+	tmp = string;
+	for (i = 0; i < *tokensCount; i++) {
+		tmp2 = strpbrk(tmp, delimiters);
+		if (tmp2 == NULL) {
+			lenToken = strlen(tmp);
+			*tokens[i] = (char*) malloc(lenToken * sizeof(char));
+			memcpy(*tokens[i], tmp, lenToken);
+			break;
+		}
+		lenToken = strlen(tmp);
+		lenToken = strlen(tmp) - strlen(tmp2);
+		*tokens[i] = (char*) malloc(lenToken * sizeof(char));
+		memcpy(*tokens[i], tmp, lenToken);
+		tmp = strpbrk(tmp, delimiters);
+		tmp = tmp + 1;
+	}
+}
