688b7e8b407a02366e5034bc06e9974fa096f827
Girka-Kirill <90462891+Girka-Kirill@users.noreply.github.com>
2021-10-15 19:34:08 +0300

    Update Girka5.c

--- Girka5.c
+++ Girka5.c
@@ -5,48 +5,56 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 
+void ChattingBetweenPrograms (char* a, char* b, char* bufferRd, char* bufferWr, int fdRd, int fdWr) {
+        pid_t result = fork();
+
+        if (result < 0) {
+                printf("Can't fork children\n");
+                exit(-1);
+        }
+
+        if (result > 0) {
+                fdWr = open(a, O_WRONLY);
+
+                while (1) {
+                        fgets(bufferWr, 100, stdin);
+                        write(fdWr, bufferWr, 100);
+                }
+        } else {
+                fdRd = open(b, O_RDONLY);
+
+                while (1) {
+                        read(fdRd, bufferRd, 100);
+                        printf("%s", bufferRd);
+                }
+        }
+}
+
 int main(int argc, char* argv[]) {
         (void)umask(0);
 
         char a[] = "a.fifo";
         char b[] = "b.fifo";
+        pid_t result = fork();
+        char bufferWr[100];
+        char bufferRd[100];
+        int fd1, fd2;
 
         if (mknod(a, S_IFIFO | 0666, 0) < 0) {
                 printf("Can't create FIFO\n");
         }
-
         if (mknod(b, S_IFIFO | 0666, 0) < 0) {
                 printf("Can't create FIFO\n");
         }
-
-        pid_t result = fork();
-
         if (result < 0) {
                 printf("fork's error\n");
                 exit(-1);
         }
-
-        int fdRd, fdWr;
         if (atoi(argv[1]) == 0) {
-                fdRd = open("a.fifo", O_RDONLY);
-                fdWr = open("b.fifo", O_WRONLY);
+                ChattingBetweenPrograms(a, b, bufferRd, bufferWr, fd1, fd2);
         }
         else if (atoi(argv[1]) == 1) {
-                fdRd = open("b.fifo", O_RDONLY);
-                fdWr = open("a.fifo", O_WRONLY);
+                ChattingBetweenPrograms(b, a, bufferRd, bufferWr, fd2, fd1);
         }
-        char bufferWr[100];
-        char bufferRd[100];
-
-        while (1) {
-                        if (result != 0) {
-                                fgets(bufferWr, 100, stdin);
-                                write(fdWr, bufferWr, 100);
-                        } else {
-                                read(fdRd, bufferRd, 100);
-                                printf("%s", bufferRd);
-                        }
-        }
-
         return 0;
 }
