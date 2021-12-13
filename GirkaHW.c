a3f96721bc2916ca43f3ce703fa5a6c9f8c737c0
Girka-Kirill <90462891+Girka-Kirill@users.noreply.github.com>
2021-12-13 15:54:35 +0300

    Update GirkaHW.c

--- GirkaHW.c
+++ GirkaHW.c
@@ -3,37 +3,29 @@
 #include <malloc.h>
 #include <stdlib.h>
 
-void Split(char* string, char* delimiters, char*** tokens, int* tokensCount) {
-	char* tmp = string;
-	char* tmp2;
-	int len = strlen(string), i = 0, lenToken;
-	while (tmp != NULL) {
-		tmp = strpbrk(tmp, delimiters);
-		i++;
-		if (tmp == NULL) {
-			break;
-		}
-		tmp = tmp + 1;
-	}
-	*tokensCount = i;
-	i = 0;
-	*tokens = (char**) realloc(*tokens, *tokensCount * sizeof(char*));
-	tmp = string;
-	for (i = 0; i < *tokensCount; i++) {
-		tmp2 = strpbrk(tmp, delimiters);
-		if (tmp2 == NULL) {
-			lenToken = strlen(tmp);
-			*tokens[i] = (char*) malloc(lenToken * sizeof(char));
-			memcpy(*tokens[i], tmp, lenToken);
-			break;
-		}
-		lenToken = strlen(tmp);
-		lenToken = strlen(tmp) - strlen(tmp2);
-		*tokens[i] = (char*) malloc(lenToken * sizeof(char));
-		memcpy(*tokens[i], tmp, lenToken);
-		tmp = strpbrk(tmp, delimiters);
-		tmp = tmp + 1;
-	}
+void Split(char* string, char* delimiters, char** tokens, int* tokensCount) {
+        string = strtok(string, delimiters);
+        while (string != NULL) {
+            tokens[*tokensCount] = string;
+            (*tokensCount)++;
+            string = strtok(NULL, delimiters);
+        }
 }
+
+int main()
+{
+    char* string = (char*) malloc(1000 * sizeof(char));
+    char* delimiters = (char*) malloc(100 * sizeof(char));
+    char** tokens = (char**) malloc(1000 * sizeof(char));
+    int tokensCount = 0, i;
+    fgets(string, 1000, stdin);
+    fgets(delimiters, 100, stdin);
+    Split(string, delimiters, tokens, &tokensCount);
+    for (i = 0; i < tokensCount; i++) {
+        printf("%s\n", tokens[i]);
+    }
+    return 0;
+}
+
 
 // TODO: продемонстрируйте в int main(), как воспользоваться ф-ей Split
